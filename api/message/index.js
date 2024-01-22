d5089fd0862ceaf6c244adc2972a15f4e521894b
Richard Iwasa <riwasa@microsoft.com>
2024-01-22 17:07:18 -0500

    Added code to show username in API response.

--- api/message/index.js
+++ api/message/index.js
@@ -10,7 +10,12 @@ module.exports = async function (context, req) {
     //     // status: 200, /* Defaults to 200 */
     //     body: responseMessage
     // };
+    const header = req.headers.get("x-ms-client-principal");
+    const encoded = Buffer.from(header, "base64");
+    const decoded = encoded.toString("ascii");
+    const user = JSON.parse(decoded);
+
     context.res.json({
-        text: "Hello from the API"
+        text: "Hello from the API, {user.userDetails}"
     });
 };
