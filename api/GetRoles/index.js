5b2eceec04e3c5c90596a3a3189802cf6bc68947
Richard Iwasa <riwasa@microsoft.com>
2024-01-22 20:30:08 -0500

    Changed code to match sample.

--- api/GetRoles/index.js
+++ api/GetRoles/index.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch';
+const fetch = require('node-fetch').default;
 
 // add role names to this object to map them to group ids in your AAD tenant
 const roleGroupMappings = {
@@ -6,7 +6,7 @@ const roleGroupMappings = {
     'reader': 'abc23d63-ee6a-4aec-9d50-1eec10e0f602'
 };
 
-export default async function (context, req) {
+module.exports = async function (context, req) {
     const user = req.body || {};
     const roles = [];
     
